c813332a2d8dd28798303e5e2b70fbf8c4e8c854
Govind1725 <128210958+Govind1725@users.noreply.github.com>
2023-03-30 18:33:25 +0530

    Update Booking.java

--- Booking.java
+++ Booking.java
@@ -1,51 +1,40 @@
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.ArrayList;
+package BusResv;
 import java.util.*;
+import java.sql.SQLException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat; 
 public class Booking {
-    Scanner sc = new Scanner(System.in);
-    String name;
-    int age;
-    int busno;
-    static Date date;
-    String dateS;
+	String passengerName;
+	int busNo;
+	Date date;
+	
+	Booking(){
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter name of passenger: ");
+		passengerName = scanner.next();
+		System.out.println("Enter bus no: ");
+		busNo = scanner.nextInt();
+		System.out.println("Enter date dd-mm-yyyy");
+		String dateInput = scanner.next();
+		SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy");
+		
+		try {
+			date = dateFormat.parse(dateInput);
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
 
-    Booking() {
-        System.out.println("Enter your name: ");
-        name = sc.nextLine();
-        System.out.println("Enter your age: ");
-        age = sc.nextInt();
-        System.out.println("Enter the bus no: ");
-        busno = sc.nextInt();
-        System.out.println("Enter the date in dd-mm-yyyy:");
-        {
-            sc.nextLine();
-            dateS = sc.nextLine();
-            SimpleDateFormat dateFormat = new SimpleDateFormat("dd MM yyyy");
-            try {
-                date = dateFormat.parse(dateS);
-            } catch (ParseException e) {
-                throw new RuntimeException(e);
-            }
-        }
-    }
+	}
+	
+	public boolean isAvailable()  throws SQLException{
+		
+		BusDAO busdao = new BusDAO();
+		BookingDAO bookingdao = new BookingDAO();
+		int capacity = busdao.getCapacity(busNo);
 
-     public boolean isAvailable(ArrayList<Booking> bookings, ArrayList<Bus> buses) {
-            int capacity = 0;
-            for (Bus bus : buses) {
-                if (bus.getBusno() == busno) {
-                    capacity = bus.getCapacity();
-                }
-            }
-                int booked = 0;
-                for (Booking b : bookings) {
-                    if (b.busno == busno && b.date.equals(date)) {
-                        booked++;
-                    }
-                }
-
-
-                return booked < capacity ? true : false;
-            }
-
-        }
+		int booked = bookingdao.getBookedCount(busNo,date);
+		
+		return booked<capacity;
+		
+	}
+}
